8c7bfaad0cac246fbc18b4c0d3871866f2d4709a
Жанна Нарсия <z.narsija@superjob.ru>
2019-10-20 21:14:16 +0300

    Переназываю файлы

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import {WelcomeScreen} from '../WelcomeScreen/welcome-screen.jsx';
+import {WelcomeScreen} from '../welcome-screen/welcome-screen.jsx';
 
 export const App = (props) => {
   const {time, mistakes, onClick} = props;
